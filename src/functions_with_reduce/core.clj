54a54fdeec743a0bd4d3a4575d3afe8fae9a20c2
Abner Carleto <abnercarleto@ymail.com>
2016-10-02 22:59:08 -0300

    doc string for my-map function

--- src/functions_with_reduce/core.clj
+++ src/functions_with_reduce/core.clj
@@ -5,7 +5,9 @@
 ;;; Reimplementing functions map, filter and some using reduce. 
 ;;; This was suggested by Daniel Higginbotham in his book named Clojure for the Brave and True in chapter 4
 
-(defn my-map [f list]
+(defn my-map 
+  "Returns a sequence consisting of the result of applying f to items of coll"
+  [f list]
   (reverse
    (reduce
     (fn [new-seq val]
